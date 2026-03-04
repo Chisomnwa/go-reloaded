71661047c3aa14727f32cd7b7e9246e2bc1797cc
cnnamani <nnamanichisomnc@gmail.com>
2026-03-04 17:30:50 +0100

    Created a case modifier function

--- functions/handleCaseModifiers.go
+++ functions/handleCaseModifiers.go
@@ -1,18 +1,12 @@
 package functions
 
 import (
-	"strings"
 	"strconv"
-
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
+	"strings"
 )
 
 func handleCaseModifiers(text string) string {
 	words := strings.Split(text, " ")
-
-	// build a caser to work on (cap)
-	caser := cases.Title(language.English)
 
 	for i := 0; i < len(words); i++ {
 		// scan single word case
@@ -23,7 +17,7 @@ func handleCaseModifiers(text string) string {
 			case "(low)":
 				words[i-1] = strings.ToLower(words[i-1])
 			case "(cap)":
-				words[i-1] = caser.String(words[i-1])
+				words[i-1] = Capitalize(words[i-1])
 			}
 
 			// remove (up), (low) and (cap)
@@ -32,42 +26,57 @@ func handleCaseModifiers(text string) string {
 		}
 
 		// Let's treat multi word Case e.g (up N)
-		if i < len(words) && 
+		if i < len(words) &&
 			(strings.HasPrefix(words[i], "(up") ||
-			strings.HasPrefix(words[i], "(low") ||
-			strings.HasPrefix(words[i], "(cap")) {
-				if i + 1 >= len(words) {
-					continue
-				}
+				strings.HasPrefix(words[i], "(low") ||
+				strings.HasPrefix(words[i], "(cap")) {
+			if i+1 >= len(words) {
+				continue
+			}
 
-				// Let's extract modifier type
-				modifier := strings.TrimPrefix(words[i], "(")
-				modifier = strings.TrimSuffix(modifier, ",") // now we have "up", "low" or "cap"
+			// Let's extract modifier type
+			modifier := strings.TrimPrefix(words[i], "(")
+			modifier = strings.TrimSuffix(modifier, ",") // now we have "up", "low" or "cap"
 
-				// Let's extract the number
-				numStr := strings.TrimSuffix(words[i + 1], ")")
-				N, err := strconv.Atoi(numStr)
-				if err != nil {
-					continue
-				}
+			// Let's extract the number
+			numStr := strings.TrimSuffix(words[i+1], ")")
+			N, err := strconv.Atoi(numStr)
+			if err != nil {
+				continue
+			}
 
-				// Let's apply modifier to previous N words
-				for j := 1; j <= N && i-j >= 0; j++ {
-					switch modifier {
-					case "up":
-						words[i-j] = strings.ToUpper(words[i - j])
-					case "low":
-						words[i-j] = strings.ToLower(words[i-j])
-					case "cap":
-						words[i-j] = caser.String(words[i-j])
-					}
+			// Let's apply modifier to previous N words
+			for j := 1; j <= N && i-j >= 0; j++ {
+				switch modifier {
+				case "up":
+					words[i-j] = strings.ToUpper(words[i-j])
+				case "low":
+					words[i-j] = strings.ToLower(words[i-j])
+				case "cap":
+					words[i-j] = Capitalize(words[i-j])
 				}
+			}
 
-				// Remove e.g "(up," "N)"
-				words = append(words[:i], words[i+2:]...)
-				i--
-				}
+			// Remove e.g "(up," "N)"
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
 	}
 
 	return strings.Join(words, " ")
+}
+
+// Create a Capitalized function
+func Capitalize(word string) string {
+	if word == "" {
+		return word
+	}
+
+	word = strings.ToLower(word)
+	runes := []rune(word)
+
+	if runes[0] >= 'a' && runes[0] <= 'z' {
+		runes[0] = runes[0] - 32
+	}
+	return string(runes)
 }
--- functions/processText.go
+++ functions/processText.go
@@ -4,9 +4,8 @@ func ProcessText(text string) string {
 	text = handleHex(text)
 	text = handleBin(text)
 	text = handleCaseModifiers(text)
-	// text = fixPunctuation(text)
-	// text = fixQuotes(text)
-	// text = fixAtoAn(text)
+	text = fixPunctuation(text)
+	text = fixAtoAn(text)
 
 	return text
 }
